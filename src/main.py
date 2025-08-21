9e703ccf8fe83a1343a54f6b96ce4e6049457b1c
vilebile_17 <ibrahimsomauroo@proton.me>
2025-08-21 19:02:01 +0100

    lol

--- src/main.py
+++ src/main.py
@@ -5,11 +5,11 @@ from extract_markdown_links import extract_title
 def get_file_list(first_run=True, current_dir="./static"):
     # these two if statements are to make sure that the public directory is empty
     if first_run:
-        if not os.path.exists("./public"):
-            os.mkdir("./public")
+        if not os.path.exists("./docs"):
+            os.mkdir("./docs")
         else:
-            shutil.rmtree("./public")
-            os.mkdir("./public")
+            shutil.rmtree("./docs")
+            os.mkdir("./docs")
 
     file_list = []
     static_files = os.listdir(current_dir)
@@ -26,15 +26,15 @@ def copy_to_public(file_list):
     for file in file_list:
         path_to_static = file.split("./static/",1)[1]
         if not "/" in path_to_static:
-            shutil.copy(file, "./public")
+            shutil.copy(file, "./docs")
         else:
-            directories_only = os.path.dirname(file.replace("static", "public"))
+            directories_only = os.path.dirname(file.replace("static", "docs"))
             os.makedirs(directories_only, exist_ok=True)
-            shutil.copy(file, file.replace("static", "public"))
+            shutil.copy(file, file.replace("static", "docs"))
 
 
 
-def generate_page(from_path, template_path, dest_path):
+def generate_page(from_path, template_path, dest_path, basepath):
     print(f"Generating page from {from_path} to {dest_path} using {template_path}")
 
     with open(from_path, "r") as f:
@@ -52,6 +52,8 @@ def generate_page(from_path, template_path, dest_path):
 
     html_page = template_content.replace("{{ Title }}", title)
     html_page = html_page.replace("{{ Content }}", html_content)
+    html_page = html_page.replace('href="/', f'href="{basepath}')
+    html_page = html_page.replace('src="/', f'src="{basepath}')
     print("Filled in the html template")
 
     directories_only = os.path.dirname(dest_path)
@@ -61,26 +63,32 @@ def generate_page(from_path, template_path, dest_path):
     print("Successfully wrote the file!\n")
 
 
-def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path, basepath):
     if os.path.isdir(dir_path_content):
         for file in os.listdir(dir_path_content):
             print(f"I'm going to go searching in {os.path.join(dest_dir_path, file)}")
-            generate_pages_recursive(os.path.join(dir_path_content, file), template_path, os.path.join(dest_dir_path, file))
+            generate_pages_recursive(os.path.join(dir_path_content, file), template_path, os.path.join(dest_dir_path, file), basepath)
     
     elif os.path.isfile(dir_path_content):
         if dir_path_content.endswith(".md"):
             relative_path = os.path.relpath(dest_dir_path, "content").replace(".md", ".html")
-            dest_path = os.path.join("public", relative_path)
+            dest_path = os.path.join("docs", relative_path)
             os.makedirs(os.path.dirname(dest_path), exist_ok=True)
-            generate_page(dir_path_content, template_path, dest_path) 
+            generate_page(dir_path_content, template_path, dest_path, basepath) 
     else:
         raise ValueError(f"Somehow, the selected object : {dir_path_content} isn't a directory nor a file")
 
     
 
     
+def main():
+    try:
+        basepath = sys.argv[1]
+    except:
+        basepath = "/"
 
+    files = get_file_list()
+    copy_to_public(files)
+    generate_pages_recursive("content", "template.html", "docs", basepath)
 
-files = get_file_list()
-copy_to_public(files)
-generate_pages_recursive("content", "template.html", "public")
+main()
